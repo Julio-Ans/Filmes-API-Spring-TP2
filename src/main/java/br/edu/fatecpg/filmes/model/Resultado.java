42e817d5e032eec1c4db56c829c937bf145d8ab8
jua <julio.mendes@fatec.sp.gov.br>
2024-09-12 00:01:42 -0300

    Ajuste na impressão

--- src/main/java/br/edu/fatecpg/filmes/model/Resultado.java
+++ src/main/java/br/edu/fatecpg/filmes/model/Resultado.java
@@ -3,11 +3,12 @@ package br.edu.fatecpg.filmes.model;
 import com.fasterxml.jackson.annotation.*;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 
 public record Resultado(
         @JsonAlias("Title") String titulo,
         @JsonAlias("Runtime") String duracao,
         @JsonAlias("Country") String pais,
-        @JsonAlias("Poster") String poster){
+        @JsonAlias("Poster") String poster,
+        @JsonAlias("Type") String tipo
+) {
 }
